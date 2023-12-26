30ffca92e95380e1db168f795ce3e35b734a6dbc
Arwindhraj <86314465+ARWINDHRAJ@users.noreply.github.com>
2023-12-26 19:31:19 +0530

    Updated

--- dataloader_first.py
+++ dataloader_first.py
@@ -1,90 +1,74 @@
-from PIL import Image
-from xml.etree import ElementTree as ET
+import torch
+from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
-import torch 
-import os
+from pycocotools.coco import COCO
+from PIL import Image
 
-class PotholeDataset(torch.utils.data.Dataset):
-    def __init__(self, root_dir, transform=None):
-        self.root_dir = root_dir
+class CustomCOCODataset(Dataset):
+    def __init__(self, annotation_file, image_root, transform=None):
+        self.coco = COCO(annotation_file)
+        self.image_root = image_root
         self.transform = transform
-        self.image_paths, self.annotation_paths = self.list_paths()  # Call list_paths to initialize paths
-        self.class_mapping = {'label1': 1,'label2': 0}
+
     def __len__(self):
-        return len(self.image_paths)
+        return len(self.coco.getImgIds())
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.root_dir, self.image_paths[idx])
-        annotation_path = os.path.join(self.root_dir, self.annotation_paths[idx])
+        img_id = self.coco.getImgIds()[idx]
+        img_info = self.coco.loadImgs(img_id)[0]
+        image_path = f"{self.image_root}/{img_info['file_name']}"
+        image = Image.open(image_path).convert("RGB")
 
-        # Load image
-        img = Image.open(img_path).convert('RGB')
+        ann_ids = self.coco.getAnnIds(imgIds=img_id)
+        annotations = self.coco.loadAnns(ann_ids)
 
-        # Parse annotation
-        annotation_tree = ET.parse(annotation_path)
-        root = annotation_tree.getroot()
-        # Extract bounding box coordinates and labels from the annotation file
-        # Example: Replace this with actual parsing logic
-        box_coords = []  # List of [xmin, ymin, xmax, ymax]
-        labels = []  # List of corresponding labels
-        for obj in root.findall('.//object'):
-            bbox = obj.find('bndbox')
-            xmin = float(bbox.find('xmin').text)
-            ymin = float(bbox.find('ymin').text)
-            xmax = float(bbox.find('xmax').text)
-            ymax = float(bbox.find('ymax').text)
-            box_coords.append([xmin, ymin, xmax, ymax])
-            labels.append(self.class_mapping[obj.find('name').text])
-        # Create target dictionary
-        target = {'boxes': torch.tensor(box_coords, dtype=torch.float32),
-                  'labels': torch.tensor(labels, dtype=torch.int64)}
+        # Process annotations
+        boxes = [ann['bbox'] for ann in annotations]
+        labels = [ann['category_id'] for ann in annotations]
 
-        # Apply transformations if provided
+        # Convert to tensor
+        boxes = torch.tensor(boxes, dtype=torch.float32)
+        labels = torch.tensor(labels, dtype=torch.int64)
+
+        targets = {
+            'boxes': boxes,
+            'labels': labels,
+        }
+
         if self.transform:
-            img, target = self.transform(img, target)
+            image = self.transform(image)
 
-        return img, target
+        return {'image': image, 'targets': targets}
     
-    def list_paths(self):
-        image_paths, annotation_paths = [], []
-        for split in ['train', 'valid', 'test']:
-            split_dir = os.path.join(self.root_dir, split)
-            for filename in os.listdir(split_dir):
-                if filename.endswith('.jpg'):
-                    image_path = os.path.join(split, filename)
-                    annotation_path = os.path.join(split, filename.replace('.jpg', '.xml'))
-                    image_paths.append(image_path)
-                    annotation_paths.append(annotation_path)
-        return image_paths, annotation_paths
+if __name__ == '__main__':
+    # Example usage
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # Adjust as needed
+        transforms.ToTensor(),
+    ])
 
-transform = transforms.Compose([
-    transforms.Resize((300, 300)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
+    # Replace 'path/to/annotations.json' and 'path/to/images' with your actual paths
+    dataset = CustomCOCODataset(annotation_file='Dataset/train/_annotations.coco.json', image_root='Dataset/train/', transform=transform)
 
-def collate_fn(batch):
-    images, targets = zip(*batch)
-    images = torch.stack(images)
-    return images, targets
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=4)  # Adjust batch size and num_workers as needed
+    print("Done")
+    # # Assuming you have created your DataLoader as 'dataloader'
+    # for batch in dataloader:
+    #     images = batch['image']
+    #     targets = batch['targets']
 
-# Replace with actual image and annotation paths
-dataset = PotholeDataset(root_dir='D:\Project_Files\Dataset', transform=transform)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True, collate_fn=collate_fn)
+    #     # Print the shapes to verify
+    #     print(f"Image batch shape: {images.shape}")
+    #     print(f"Boxes batch shape: {targets['boxes'].shape}")
+    #     print(f"Labels batch shape: {targets['labels'].shape}")
 
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+    #     # Print the first image and its corresponding annotations
+    #     print("First Image:")
+    #     print(images[0])
+    #     print("Annotations:")
+    #     print("Boxes:", targets['boxes'][0])
+    #     print("Labels:", targets['labels'][0])
 
-def visualize_sample(img, target):
-    plt.imshow(img.permute(1, 2, 0))  # Change the image tensor layout
+    #     # Break the loop after printing the first batch
+    #     break
 
-    # Adjust this part based on the structure of your target
-    for box in target['boxes']:
-        rect = patches.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1], linewidth=1, edgecolor='r', facecolor='none')
-        plt.gca().add_patch(rect)
-
-    plt.show()
-
-# Example usage
-sample_img, sample_target = dataset[0]
-visualize_sample(sample_img, sample_target)
---
+++ main_with_data.py
@@ -0,0 +1,151 @@
+import torch
+from torch import nn
+from torch import save 
+from torch.optim import SGD
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from pycocotools.coco import COCO
+from PIL import Image
+from torch.optim.lr_scheduler import StepLR
+
+class CustomCOCODataset(Dataset):
+    def __init__(self, annotation_file, image_root, transform=None):
+        self.coco = COCO(annotation_file)
+        self.image_root = image_root
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.coco.getImgIds())
+
+    def __getitem__(self, idx):
+        img_id = self.coco.getImgIds()[idx]
+        img_info = self.coco.loadImgs(img_id)[0]
+        image_path = f"{self.image_root}/{img_info['file_name']}"
+        image = Image.open(image_path).convert("RGB")
+
+        ann_ids = self.coco.getAnnIds(imgIds=img_id)
+        annotations = self.coco.loadAnns(ann_ids)
+
+        # Process annotations
+        boxes = [ann['bbox'] for ann in annotations]
+        labels = [ann['category_id'] for ann in annotations]
+        
+        # Pad bounding boxes to a fixed size
+        max_num_boxes = 4
+        while len(boxes) < max_num_boxes:
+            boxes.append([0, 0, 0, 0])  # You can adjust this padding as needed
+        # Convert to tensor
+        boxes = torch.tensor(boxes, dtype=torch.float32)
+        labels = torch.tensor(labels, dtype=torch.int64)
+
+        targets = {
+            'boxes': boxes,
+            'labels': labels,
+        }
+
+        if self.transform:
+            image = self.transform(image)
+
+        return {'image': image, 'targets': targets}
+
+class MyArch(nn.Module):
+    def __init__(self):
+        super(MyArch, self).__init__()
+        
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.silu1 = nn.SiLU()
+        
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.silu2 = nn.SiLU()
+
+        self.conv3 = nn.Conv2d(64,128,kernel_size=3,padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.silu3 = nn.SiLU()
+
+        self.conv4 = nn.Conv2d(128,128,kernel_size=3,padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.silu4 = nn.SiLU()
+
+        self.conv5 = nn.Conv2d(128,256,kernel_size=3,padding=1)
+        self.bn5 = nn.BatchNorm2d(256)
+        self.silu5 = nn.SiLU()
+
+        self.conv6 = nn.Conv2d(256,256,kernel_size=3,padding=1)
+        self.bn6 = nn.BatchNorm2d(256)
+        self.silu6 = nn.SiLU()
+
+        self.conv7 = nn.Conv2d(256,512,kernel_size=3,padding=1)
+        self.bn7 = nn.BatchNorm2d(512)
+        self.silu7 = nn.SiLU()
+
+        self.conv8 = nn.Conv2d(512,512,kernel_size=3,padding=1)
+        self.bn8 = nn.BatchNorm2d(512)
+        self.silu8 = nn.SiLU()
+
+        self.conv9 = nn.Conv2d(512,1024,kernel_size=3,padding=6)
+        self.bn9 = nn.BatchNorm2d(1024)
+        self.silu9 = nn.SiLU()
+
+        self.conv10 = nn.Conv2d(1024,1024,kernel_size=3)
+        self.bn10 = nn.BatchNorm2d(1024)
+        self.silu10 = nn.SiLU()
+        
+    def forward(self, x):
+        input = x
+        
+        x1 = self.silu1(self.bn1(self.conv1(x)))
+        x2 = self.silu2(self.bn2(self.conv2(x1)))
+        x2 += x1
+        x3 = self.silu3(self.bn3(self.conv3(x2)))
+        x3 += x2
+        x4 = self.silu4(self.bn4(self.conv4(x3)))
+        x4 += x3
+        x5 = self.silu5(self.bn5(self.conv5(x4)))
+        x5 += x4
+        x6 = self.silu6(self.bn6(self.conv6(x5)))
+        x6 += x5
+        x7 = self.silu7(self.bn7(self.conv7(x6)))
+        x7 += x6
+        x8 = self.silu8(self.bn8(self.conv8(x7)))
+        x8 += x7
+        x9 = self.silu9(self.bn9(self.conv9(x8)))
+        x9 += x8
+        x10 = self.silu10(self.bn10(self.conv10(x9)))
+
+        # Gradient Flow (efficient propagation of gradients)
+        return x10
+    
+if __name__ == "__main__": 
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    dataset = CustomCOCODataset(annotation_file='Dataset/train/_annotations.coco.json', image_root='Dataset/train/', transform=transform)
+
+    dataloader = DataLoader(dataset, batch_size=8, shuffle=True, num_workers=4)
+    print("Done")
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    clf = MyArch().to(device)
+    optimizer = SGD(clf.parameters(), lr=0.001, momentum=0.9)
+    scheduler = StepLR(optimizer, step_size=5, gamma=0.1)
+    loss_function = nn.SmoothL1Loss()
+
+    epochs = 10 
+    for epoch in range(epochs):
+        clf.train() 
+
+        for data in dataloader:
+            inputs, targets = data
+            inputs, targets = inputs.to('cuda'), targets.to('cuda')
+
+            optimizer.zero_grad()
+            outputs = clf(inputs)
+            loss = loss_function(outputs, targets)
+            loss.backward()
+            optimizer.step()
+
+        print(f'Epoch {epoch + 1}/{epochs}, Loss: {loss.item()}')
