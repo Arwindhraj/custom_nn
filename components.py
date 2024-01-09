847b391d2c406f4d0a9c7a2044b23a10c023a099
Arwindhraj <86314465+ARWINDHRAJ@users.noreply.github.com>
2024-01-09 21:18:12 +0530

    Code Updated

--- components.py
+++ components.py
@@ -1,18 +1,9 @@
-import os
 import torch
 import numpy as np
-import pandas as pd
-from PIL import Image
-from torch import save
-from torch.optim import SGD
 from torch import nn, Tensor
 import torchvision.ops as ops
 from typing import Tuple, List
 from torch.nn import functional as F
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
-import torchvision.transforms as transforms
-from torch.optim.lr_scheduler import StepLR
 
 class BBox(object):
 
@@ -24,16 +15,13 @@ class BBox(object):
         self.right = right
         self.bottom = bottom
 
-    
     def __repr__(self) -> str:
         return 'BBox[l={:.1f}, t={:.1f}, r={:.1f}, b={:.1f}]'.format(
             self.left, self.top, self.right, self.bottom)
 
-    
     def tolist(self):
         return [self.left, self.top, self.right, self.bottom]
 
-    
     @staticmethod
     def to_center_base(bboxes: Tensor):
         return torch.stack([
@@ -43,7 +31,6 @@ class BBox(object):
             bboxes[:, 3] - bboxes[:, 1]
         ], dim=1)
 
-    
     @staticmethod
     def from_center_base(center_based_bboxes: Tensor) -> Tensor:
         return torch.stack([
@@ -52,10 +39,7 @@ class BBox(object):
             center_based_bboxes[:, 0] + center_based_bboxes[:, 2] / 2,
             center_based_bboxes[:, 1] + center_based_bboxes[:, 3] / 2
         ], dim=1)
-
-    
-    
-    
+        
     @staticmethod
     def calc_transformer(src_bboxes: Tensor, dst_bboxes: Tensor) -> Tensor:
         center_based_src_bboxes = BBox.to_center_base(src_bboxes)
@@ -67,8 +51,7 @@ class BBox(object):
             torch.log(center_based_dst_bboxes[:, 3] / center_based_src_bboxes[:, 3])
         ], dim=1)
         return transformers
-
-    
+   
     @staticmethod
     def apply_transformer(src_bboxes: Tensor, transformers: Tensor) -> Tensor:
         center_based_src_bboxes = BBox.to_center_base(src_bboxes)
@@ -80,8 +63,7 @@ class BBox(object):
         ], dim=1)
         dst_bboxes = BBox.from_center_base(center_based_dst_bboxes)
         return dst_bboxes
-
-    
+   
     @staticmethod
     def iou(source: Tensor, other: Tensor) -> Tensor:
         source = source.repeat(other.shape[0], 1, 1).permute(1, 0, 2)
@@ -99,16 +81,14 @@ class BBox(object):
         intersection_area = intersection_width * intersection_height
 
         return intersection_area / (source_area + other_area - intersection_area)
-
-    
+   
     @staticmethod
     def inside(source: Tensor, other: Tensor) -> bool:
         source = source.repeat(other.shape[0], 1, 1).permute(1, 0, 2)
         other = other.repeat(source.shape[0], 1, 1)
         return ((source[:, :, 0] >= other[:, :, 0]) * (source[:, :, 1] >= other[:, :, 1]) *
                 (source[:, :, 2] <= other[:, :, 2]) * (source[:, :, 3] <= other[:, :, 3]))
-
-    
+   
     @staticmethod
     def clip(bboxes: Tensor, left: float, top: float, right: float, bottom: float) -> Tensor:
         return torch.stack([
@@ -127,33 +107,27 @@ class RPN(nn.Module):
             nn.Conv2d(in_channels=num_features_out, out_channels=512, kernel_size=3, padding=1),
             nn.ReLU()
         )
-
-        
+       
         self._anchor_ratios = anchor_ratios
         self._anchor_scales = anchor_scales
 
         num_anchor_ratios = len(self._anchor_ratios)
         num_anchor_scales = len(self._anchor_scales)
         num_anchors = num_anchor_ratios * num_anchor_scales
-
         
         self._pre_nms_top_n = pre_nms_top_n
         self._post_nms_top_n = post_nms_top_n
 
-
-        
         self._objectness = nn.Conv2d(in_channels=512, out_channels=num_anchors * 2, kernel_size=1)
         self._transformer = nn.Conv2d(in_channels=512, out_channels=num_anchors * 4, kernel_size=1)
 
     def forward(self, features: Tensor, image_width: int, image_height: int) -> Tuple[Tensor, Tensor]:
         
         features = self._features(features)
-
         
         objectnesses = self._objectness(features)
         transformers = self._transformer(features)
-
-        
+       
         objectnesses = objectnesses.permute(0, 2, 3, 1).contiguous().view(-1, 2)
         
         transformers = transformers.permute(0, 2, 3, 1).contiguous().view(-1, 4)
@@ -161,15 +135,12 @@ class RPN(nn.Module):
         return objectnesses, transformers
 
     def sample(self, anchor_bboxes: Tensor, gt_bboxes: Tensor, image_width: int, image_height: int) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
-
-        
+       
         sample_fg_indices = torch.arange(end=len(anchor_bboxes), dtype=torch.long)
         sample_selected_indices = torch.arange(end=len(anchor_bboxes), dtype=torch.long)
-
-        
+       
         anchor_bboxes = anchor_bboxes.cpu()
         gt_bboxes = gt_bboxes.cpu()
-
         
         boundary = torch.tensor(BBox(0, 0, image_width, image_height).tolist(), dtype=torch.float)
         inside_indices = BBox.inside(anchor_bboxes, boundary.unsqueeze(dim=0)).squeeze().nonzero().view(-1)
@@ -177,8 +148,7 @@ class RPN(nn.Module):
         anchor_bboxes = anchor_bboxes[inside_indices]
         sample_fg_indices = sample_fg_indices[inside_indices]
         sample_selected_indices = sample_selected_indices[inside_indices]
-
-        
+     
         labels = torch.ones(len(anchor_bboxes), dtype=torch.long) * -1
         ious = BBox.iou(anchor_bboxes, gt_bboxes)
         anchor_max_ious, anchor_assignments = ious.max(dim=1)
@@ -187,30 +157,22 @@ class RPN(nn.Module):
         labels[anchor_max_ious < 0.3] = 0
         labels[anchor_additions] = 1
         labels[anchor_max_ious >= 0.7] = 1
-
-        
-        
-        
+       
         fg_indices = (labels == 1).nonzero().view(-1)
         bg_indices = (labels == 0).nonzero().view(-1)
         fg_indices = fg_indices[torch.randperm(len(fg_indices))[:min(len(fg_indices), 128)]]
         bg_indices = bg_indices[torch.randperm(len(bg_indices))[:256 - len(fg_indices)]]
         selected_indices = torch.cat([fg_indices, bg_indices])
         selected_indices = selected_indices[torch.randperm(len(selected_indices))]
-
-        
-        
+       
         gt_anchor_objectnesses = labels[selected_indices]
         gt_bboxes = gt_bboxes[anchor_assignments[fg_indices]]
         anchor_bboxes = anchor_bboxes[fg_indices]
         gt_anchor_transformers = BBox.calc_transformer(anchor_bboxes, gt_bboxes)
-
         
         gt_anchor_objectnesses = gt_anchor_objectnesses.cuda()
         gt_anchor_transformers = gt_anchor_transformers.cuda()
-
-        
-        
+       
         sample_fg_indices = sample_fg_indices[fg_indices]
         sample_selected_indices = sample_selected_indices[selected_indices]
 
@@ -219,7 +181,6 @@ class RPN(nn.Module):
     def loss(self, anchor_objectnesses: Tensor, anchor_transformers: Tensor, gt_anchor_objectnesses: Tensor, gt_anchor_transformers: Tensor) -> Tuple[Tensor, Tensor]:
         
         cross_entropy = F.cross_entropy(input=anchor_objectnesses, target=gt_anchor_objectnesses)
-
         
         smooth_l1_loss = F.smooth_l1_loss(input=anchor_transformers, target=gt_anchor_transformers, reduction='sum')
         
@@ -228,56 +189,43 @@ class RPN(nn.Module):
         return cross_entropy, smooth_l1_loss
 
     def generate_anchors(self, image_width: int, image_height: int, num_x_anchors: int, num_y_anchors: int, anchor_size: int) -> Tensor:
-
-        
+      
         center_ys = np.linspace(start=0, stop=image_height, num=num_y_anchors + 2)[1:-1]
         center_xs = np.linspace(start=0, stop=image_width, num=num_x_anchors + 2)[1:-1]
-
-        
+      
         ratios = np.array(self._anchor_ratios)
         ratios = ratios[:, 0] / ratios[:, 1]
         scales = np.array(self._anchor_scales)
 
-        
         center_ys, center_xs, ratios, scales = np.meshgrid(center_ys, center_xs, ratios, scales, indexing='ij')
-
-        
+   
         center_ys = center_ys.reshape(-1)
         center_xs = center_xs.reshape(-1)
         ratios = ratios.reshape(-1)
         scales = scales.reshape(-1)
-
-        
+     
         widths = anchor_size * scales * np.sqrt(1 / ratios)
         heights = anchor_size * scales * np.sqrt(ratios)
-
-        
+      
         center_based_anchor_bboxes = np.stack((center_xs, center_ys, widths, heights), axis=1)
         center_based_anchor_bboxes = torch.from_numpy(center_based_anchor_bboxes).float()
-
-        
+      
         anchor_bboxes = BBox.from_center_base(center_based_anchor_bboxes)
 
         return anchor_bboxes
-
-    
+  
     def generate_proposals(self, anchor_bboxes: Tensor, objectnesses: Tensor, transformers: Tensor, image_width: int, image_height: int) -> Tensor:
-
-        
+       
         proposal_score = objectnesses[:, 1]
-
-        
+       
         _, sorted_indices = torch.sort(proposal_score, dim=0, descending=True)
         sorted_transformers = transformers[sorted_indices]
         sorted_anchor_bboxes = anchor_bboxes[sorted_indices]
-        
-        
+              
         proposal_bboxes = BBox.apply_transformer(sorted_anchor_bboxes, sorted_transformers.detach())
-        
-        
+               
         proposal_bboxes = BBox.clip(proposal_bboxes, 0, 0, image_width, image_height)
-
-        
+       
         keep_indices = ops.nms(proposal_bboxes, proposal_score, iou_threshold=0.7)
 
         proposal_bboxes = proposal_bboxes[keep_indices]
@@ -294,11 +242,9 @@ class SiLU(nn.Module):
 class MyArchitecture(nn.Module):
     def __init__(self, num_classes=1,anchor_ratios=[(1, 1), (1, 2), (2, 1)], anchor_scales=[1, 2, 4], pre_nms_top_n=1000, post_nms_top_n=200):
         super(MyArchitecture, self).__init__()
-
         
         self.silu = SiLU()
-
-        
+      
         self.conv1 = nn.Conv2d(in_channels=3,out_channels=32,kernel_size=3,stride=1,padding=1)
         self.bn1 = nn.BatchNorm2d(32)
 
@@ -325,16 +271,13 @@ class MyArchitecture(nn.Module):
 
         self.conv9 = nn.Conv2d(in_channels=256,out_channels=512,kernel_size=3,padding=1)
         self.bn9 = nn.BatchNorm2d(512)
-        
-        
+               
         self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-
-        
+       
         self.fc_obj = nn.Linear(512, 1)
         
         self.fc_cls = nn.Linear(512, num_classes)
-
-
+        
         self.rpn = RPN(num_features_out=512,anchor_ratios=anchor_ratios,anchor_scales=anchor_scales,pre_nms_top_n=pre_nms_top_n,post_nms_top_n=post_nms_top_n)
     
     def forward(self,x):
